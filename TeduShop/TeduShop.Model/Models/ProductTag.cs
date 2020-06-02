52f0e4c36c9f70afb20cb037d096029664b741fb
anh khoa DAMDANG <anhkhoait97@gmail.com>
2020-06-03 01:23:10 +0700

    code model bai so 5

---
+++ TeduShop/TeduShop.Model/Models/ProductTag.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeduShop.Model.Models
+{
+    [Table("ProducTags")]
+    public class ProductTag
+    {
+        [Key]
+        [Column(Order = 1)]
+        public int ProductID { get; set; }
+        [Key]
+        [Column(Order = 2)]
+        public string TagID { get; set; }
+        public virtual Product Product { get; set; }
+        public virtual Tag Tag { get; set; }
+    }
+}
