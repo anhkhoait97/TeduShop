70e1b0c2fa995e3250e6c60dc8b2dcbbc47e89bf
anh khoa DAMDANG <anhkhoait97@gmail.com>
2020-06-07 21:33:16 +0700

    Code bai 7, Repositorybase va cac sub repository

--- TeduShop/TeduShop.Data/Infrastucture/IUnitOfWork.cs
+++ TeduShop/TeduShop.Data/Infrastucture/IUnitOfWork.cs
@@ -4,9 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TeduShop.Data
+namespace TeduShop.Data.Infrastucture
 {
-    public class Class1
+    public interface IUnitOfWork
     {
+        void Commit();
     }
 }
